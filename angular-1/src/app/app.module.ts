0bda2d34886164786592d99694cea36de364fb66
leticiareginajf <leticia.reginajf@gmail.com>
2024-04-16 15:15:50 -0300

    Incluindo bootstrap

--- angular-1/src/app/app.module.ts
+++ angular-1/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, Comp2Component]
 })
 export class AppModule { }
