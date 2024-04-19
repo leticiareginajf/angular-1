fe0156aecfd900750596640eaf6084eb25aa5c2b
leticiareginajf <leticia.reginajf@gmail.com>
2024-04-19 14:48:05 -0300

    cards-cancel

--- angular-1/src/app/app.module.ts
+++ angular-1/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { CardsModule } from './cards/cards.module';
-import { InlineComponent } from './inline/inline.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InlineComponent,
   ],
   imports: [
     BrowserModule,
