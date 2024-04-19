bd025666684c9bdff4198faec7af62d94f8496db
leticiareginajf <leticia.reginajf@gmail.com>
2024-04-19 14:39:23 -0300

    inline template

--- angular-1/src/app/card-button/card-button.component.ts
+++ angular-1/src/app/card-button/card-button.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button',
   //templateUrl: './card-button.component.html',
-  template: '<div class="card-button">Adquirir</div>',
+  template: '<div class="card-button">TESTE</div>',
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
