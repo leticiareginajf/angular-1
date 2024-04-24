28595d8f4778fc600aa8ad38f3424e7447eb3722
leticiareginajf <leticia.reginajf@gmail.com>
2024-04-23 23:23:22 -0300

    ng deep

--- angular-1/src/app/card-button-cancel/card-button-cancel.component.ts
+++ angular-1/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss', '../card/card.component.scss'],
+  styleUrls: ['./card-button-cancel.component.scss'],
+  styles: ['div.card-cancel-button{ background-color: green;}'],
 })
 export class CardButtonCancelComponent {
 
